2105b959113e91baa5e3bee1cf3f45899e14dbbd
Digital-Invitation <ffburiq1987@gmail.com>
2025-01-29 23:07:24 +0700

    add file error navbar

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// MUSIC
+var tempMusic = ''
+music = document.querySelector('.music')
+
+if(tempMusic){
+    music.src = tempMusic
+}
+
+// MUSIC BUTTON
+var isPlaying = true
+
+function toggleMusic(event){
+    event.preventDefault()
+
+    const musicButton = document.getElementById('music-button')
+
+    if (isPlaying){
+        musicButton.innerHTML = '<i class="fas fa-fw fa-pause"></i>'
+        musicButton.classList.remove('rotate')
+        musicButton.style.transform = 'translateY(0)'
+        music.pause()
+    } else{
+        musicButton.innerHTML = '<i class="fas fa-fw fa-compact-disc"></i>'
+        musicButton.classList.add('rotate')
+        music.play()
+    }
+
+    isPlaying = !isPlaying
+}
