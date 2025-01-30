78dde6ee2b523fb05f32b35a45f917fd6d8e6e26
Digital-Invitation <ffburiq1987@gmail.com>
2025-01-30 22:30:08 +0700

    pembuka undangan

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,56 @@ function toggleMusic(event){
 
     isPlaying = !isPlaying
 }
+
+// COUNTDOWN WEDDING
+var countdownDate = new Date("Jun 29, 2025 08:00").getTime()
+var x = setInterval(function(){
+    var now = new Date().getTime()
+    var distance = countdownDate - now
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    var minutes = Math.floor((distance % (1000 * 60 * 60 )) / (1000*60))
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+    document.getElementById('countdown-wedding').innerHTML = `
+     <!-- HARI -->
+    <div class="col-lg-1 col-3">
+        <div class="text-center p-2 rounded text-light">
+            <h5>${days}</h5>Hari
+        </div>
+    </div>
+    <!-- JAM -->
+    <div class="col-lg-1 col-3">
+        <div class="text-center p-2 rounded text-light">
+            <h5>${hours}</h5>Jam
+        </div>
+    </div>
+    <!-- MENIT -->
+    <div class="col-lg-1 col-3">
+        <div class="text-center p-2 rounded text-light">
+            <h5>${minutes}</h5>Menit
+        </div>
+    </div>
+    <!-- DETIK -->
+    <div class="col-lg-1 col-3">
+        <div class="text-center p-2 rounded text-light">
+            <h5>${seconds}</h5>Detik
+        </div>
+    </div>
+    `
+
+    if (distance < 0){
+        clearInterval(x)
+        document.getElementById('countdown-wedding').innerHTML = `
+        <span class="text-center p-3 rounded text-light m-2">
+            <h2>Acara sudah dimulai</h2>
+        </span>`
+    }
+}, 1000)
+
+// NAMA SAMBUTAN
+// const urlParams = new URLSearchParams(window.location.search)
+// const panggilan = urlParams.get('p')
+// const nama = urlParams.get('n')
+// const namaSambutan = document.querySelector('#nama-sambutan')
+// namaSambutan.innerText = `${panggilan} ${nama},`
